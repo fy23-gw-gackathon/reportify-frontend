0fca65582135555b0b8782633f71fcf44c73a55b
harrisonowl <hyzitbios@hotmail.com>
2023-05-03 17:26:13 +0900

    ref #4 replace img to Image

--- src/components/commons/imageButton.tsx
+++ src/components/commons/imageButton.tsx
@@ -1,14 +1,16 @@
+import Image from "next/image";
+
 type Props = {
-  src: string;
-  width: number;
-  height: number;
-  onClick: () => void;
+    src: string;
+    width: number;
+    height: number;
+    onClick: () => void;
 };
 
 export const ImageButton = ({ src, width, height, onClick }: Props) => {
-  return (
-    <button onClick={onClick}>
-      <img src={src} width={width} height={height}></img>
-    </button>
-  );
+    return (
+        <button onClick={onClick}>
+            <Image src={src} width={width} height={height} alt=""></Image>
+        </button>
+    );
 };
