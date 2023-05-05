6846a1550d37057515aa55220f3d2fe7436b32ba
averak <averak.jp@gmail.com>
2023-05-05 15:14:01 +0900

    #25 APIクライアントをアプリケーションから呼び出し可能にした

---
+++ src/utils/api-client.ts
@@ -0,0 +1,21 @@
+import aspida from "@aspida/axios";
+import axios from "axios";
+
+import api from "@api/$api";
+
+// TODO: エラーレスポンスが返ってきた場合にToastを表示したい
+// https://stackoverflow.com/questions/55149816/is-it-possible-to-use-toast-in-axios-interceptors
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const response = error.response.data;
+        const message = response.message ? response.message : "予期せぬエラーが発生しました。";
+
+        return Promise.reject(message);
+    }
+);
+
+const ApiClient = api(aspida(axios, {}));
+export default ApiClient;
